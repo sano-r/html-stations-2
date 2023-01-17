069703d44b53041811b44ecd3e82a7b66cdea6a6
r-sano <r.sano0w0@gmail.com>
2023-01-17 12:53:55 +0900

    230117 commit

--- src/scripts/station12-common.js
+++ src/scripts/station12-common.js
@@ -1,6 +1,9 @@
 function fetchApi() {
   const url = "https://dog.ceo/api/breeds/image/random";
   fetch(url)
+    .then(response => {
+      return response.json();
+    })
     .then(() => console.log("fetch completed"))
     .catch(() => console.error("fetch failed"));
 }
